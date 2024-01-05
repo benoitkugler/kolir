6599428ea87d752d382d48fb1bed4a9ff1ee0f45
Benoit KUGLER <benoit.kugler@gmail.com>
2024-01-05 14:20:36 +0100

    better matieres export

--- kolir/lib/logic/export/matieres.dart
+++ kolir/lib/logic/export/matieres.dart
@@ -9,28 +9,45 @@ String matieresToHTML(Colloscope col) {
 
   final List<String> pages = [];
   for (var item in matieres.entries) {
+    final creneaux = item.value;
     final matiere = col.matieresList.values[item.key];
 
-    final rows = item.value.map((semaine) {
-      final creneaux = semaine.item.map((e) {
-        final time = col.semaines.dateFor(e.date.semaine, e.date.weekday);
-        return "<div class='chip'><i>${e.colleur}</i><br/><b>${e.groupe?.name ?? ''}</b> ${formatDateTime(time, e.date)}</div>";
+    final colleursSet = <String>{};
+    for (var semaine in creneaux) {
+      colleursSet.addAll(semaine.item.map((e) => e.colleur));
+    }
+    final colleurs = colleursSet.toList();
+    colleurs.sort();
+
+    final rows = creneaux.map((semaine) {
+      final byColleur = <String, List<PopulatedCreneau>>{};
+      for (var creneau in semaine.item) {
+        final l = byColleur.putIfAbsent(creneau.colleur, () => []);
+        l.add(creneau);
+      }
+      final cells = colleurs.map((colleur) {
+        final creneaux = (byColleur[colleur] ?? []).map((e) {
+          final time = col.semaines.dateFor(e.date.semaine, e.date.weekday);
+          return "<div class='chip'><b>${e.groupe?.name ?? ''}</b><br/>${formatDateTime(time, e.date)}</div>";
+        });
+        return "<td>${creneaux.join('')}</td>";
       });
       return """
       <tr>
         <td>S ${semaine.semaine}</td>
-        <td>${creneaux.join("")}</td>
+        ${cells.join("\n")}
       </tr>
       """;
     });
 
+    final colleurHeaders = colleurs.map((e) => "<th>$e</th>").join("\n");
     final page = """
     <h1>${matiere.format()}</h1>
 
     <table>
       <tr>
         <th>Semaine</th>
-        <th>Groupes</th>
+        $colleurHeaders
       </tr>
       ${rows.join("\n")}
     </table>
